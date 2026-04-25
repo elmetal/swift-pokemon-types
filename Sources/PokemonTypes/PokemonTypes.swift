0d59be1b6ca739b14c093ee8e0415e447029504c
elmetal <nm7.pupu@gmail.com>
2026-04-26 08:54:47 +0900

    PokemonType

---
+++ Sources/PokemonTypes/PokemonTypes.swift
@@ -0,0 +1,28 @@
+//
+//  PokemonType.swift
+//  swift-pokemon-damage-calculator
+//
+//  Created by elmetal on 2026/04/26
+//
+//
+
+public enum PokemonType: Equatable {
+    case normal
+    case fighting
+    case flying
+    case poison
+    case bug
+    case steel
+    case psychic
+    case ice
+    case dragon
+    case dark
+    case fairy
+    case fire
+    case water
+    case grass
+    case electric
+    case ground
+    case rock
+    case ghost
+}
