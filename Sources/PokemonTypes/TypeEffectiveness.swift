5c2b864edcdf0773797bf5363fef7ae6a33edf37
elmetal <nm7.pupu@gmail.com>
2026-04-26 09:31:49 +0900

    TypeEffectiveness

---
+++ Sources/PokemonTypes/TypeEffectiveness.swift
@@ -0,0 +1,14 @@
+//
+//  TypeEffectiveness.swift
+//  swift-pokemon-types
+//  
+//  Created by elmetal on 2026/04/26
+//  
+//
+
+public enum TypeEffectiveness: Equatable, CaseIterable, Sendable {
+    case ineffective
+    case notVeryEffective
+    case neutral
+    case superEffective
+}
