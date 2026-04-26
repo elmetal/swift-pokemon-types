6fb29c5f2518f477fbede833936f1dee55e82993
elmetal <nm7.pupu@gmail.com>
2026-04-26 09:14:37 +0900

    add PokemonTypeSet

--- Sources/PokemonTypes/PokemonType.swift
+++ Sources/PokemonTypes/PokemonType.swift
@@ -6,7 +6,7 @@
 //
 //
 
-public enum PokemonType: Equatable {
+public enum PokemonType: Equatable, CaseIterable, Sendable {
     case normal
     case fighting
     case flying
---
+++ Sources/PokemonTypes/PokemonTypeSet.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTypeSet.swift
+//  swift-pokemon-types
+//  
+//  Created by elmetal on 2026/04/26
+//  
+//
+
+public struct PokemonTypeSet: Hashable, Sendable {
+    private var storage: Set<PokemonType>
+    
+    public init(_ types: some Sequence<PokemonType>) {
+        self.storage = Set(types)
+    }
+    
+    public static let latest: Self = .init(PokemonType.allCases)
+}
