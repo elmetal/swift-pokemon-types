b67df3473994acb6c0c3c1ddfcd6e4e81ee4f080
elmetal <nm7.pupu@gmail.com>
2026-04-26 09:31:25 +0900

    fix test build

--- Tests/PokemonTypesTests/swift_pokemon_typesTests.swift
+++ Tests/PokemonTypesTests/swift_pokemon_typesTests.swift
@@ -1,5 +1,5 @@
 import Testing
-@testable import swift_pokemon_types
+@testable import PokemonTypes
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
